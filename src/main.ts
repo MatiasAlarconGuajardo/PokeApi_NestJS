7cfdf47543d12490f4cd2b8f90885766df4eb366
Matias Alarcon <matias1998@hotmail.com>
2024-04-16 12:55:12 -0400

    Actualizar configuración de Swagger y dependencias de Swagger en el proyecto

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Pokemon API')
+    .setDescription('API for Pokemon')
+    .setVersion('1.0')
+    .addTag('pokemon')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(configService.get('PORT'));
 }
--- src/pokemon/controllers/pokemon.controller.ts
+++ src/pokemon/controllers/pokemon.controller.ts
@@ -1,39 +1,44 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PokemonService } from '../services/pokemon.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
-
+  @ApiTags('pokemon')
   @Get('list/:skip?')
   public async getPokemonInfo(@Param('skip') skip: number = 0) {
     const results = await this.pokemonService.getPokemonInfo(skip);
     return { results };
   }
-
+  @ApiTags('pokemon')
   @Get(':id')
   public async getPokemonInfoByID(@Param('id') id: number) {
     return await this.pokemonService.getPokemonInfoByID(id);
   }
+  @ApiTags('Types')
   @Get('type/:id')
   public async getPokemonTypesById(@Param('id') id: number) {
     return await this.pokemonService.getPokemonTypesById(id);
   }
+  @ApiTags('Types')
+  @Get('types/:id')
+  public async getTypeNameById(@Param('id') id: number) {
+    return await this.pokemonService.getTypeNameById(id);
+  }
+  @ApiTags('Abilities')
   @Get('abilities/:id')
   public async getPokemonAbilitiesById(@Param('id') id: number) {
     return await this.pokemonService.getPokemonAbilitiesById(id);
   }
+  @ApiTags('Abilities')
   @Get('ability/:id')
   public async getAbilityNameById(@Param('id') id: number) {
     return await this.pokemonService.getAbilityNameById(id);
   }
+  @ApiTags('FlavorText')
   @Get('FlavorText/:id')
   public async getPokemonFlavorTextByID(@Param('id') id: number) {
     return await this.pokemonService.getPokemonFlavorTextByID(id);
-  }
-
-  @Get('types/:id')
-  public async getTypeNameById(@Param('id') id: number) {
-    return await this.pokemonService.getTypeNameById(id);
   }
 }
